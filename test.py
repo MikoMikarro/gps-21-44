cc5496317c832522d96bc1fb4325240596ee897b
MikoMikarro <lopezcuestam@gmail.com>
2021-12-21 15:22:01 +0100

    Update test.py

--- test.py
+++ test.py
@@ -15,12 +15,6 @@ class TestStringMethods(unittest.TestCase):
     def test_is_capitalize(self):
         sting = transform.to_capitalize("HELLO")
         self.assertEqual(sting, "Hello")
-        
-        
-    def test_is_capitalize2(self):
-        sting = transform.to_capitalize("hello")
-        self.assertEqual(sting, "Hello")
-
 
 if __name__ == '__main__':
     unittest.main()
